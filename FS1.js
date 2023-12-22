b60d608e2573bcf0f395434fa30a43715fdd9867
Pranaav <pranaavrr@gmail.com>
2023-12-22 10:10:30 +0530

    FS1.js

--- FS1.js
+++ FS1.js
@@ -1,19 +1,21 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const formidable = require('formidable');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 const app = express();
 const port = 3000;
 
+const jwtSecret = 'shhhhh';
+
 app.use(express.json());
 app.use(cors());
 
-
-
-mongoose.connect('mongodb://0.0.0.0:27017/project', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/project', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB!!');
   })
@@ -21,109 +23,228 @@ mongoose.connect('mongodb://0.0.0.0:27017/project', { useNewUrlParser: true, use
     console.error('Error in Connection!!', error);
   });
 
-
 const userSchema = new mongoose.Schema({
-  firstname: {type: String, default : ""},
-  lastname: {type: String, default : ""},
-  email: {type: String, default : ""},
-  password: {type: String, default : ""}
+  firstname: { type: String, default: '' },
+  lastname: { type: String, default: '' },
+  email: { type: String, default: '' },
+  password: { type: String, default: '' },
+  resetToken: { type: String },
+  resetTokenExpiry: { type: Date },
 });
 
 const User = mongoose.model('User', userSchema);
 
-// JWT Secret Key (change this to a secret key of your choice)
-//const jwtSecret = 'your_jwt_secret_key';
-
 // Registration Endpoint
 app.post('/register', async (req, res) => {
- try{
-    const {firstname, lastname, email, password } = req.fields;
-if(! (firstname && lastname && email && password )) {
-  res.status(400).send("Enter all the Credentials!!")
-}
-else{
-  res.send(req.fields) 
-}
+  try {
+    const form = new formidable.IncomingForm();
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user
-    const newUser = new User({
-      firstname, 
-      lastname, 
-      email,
-      password: hashedPassword,
-    });
-
-    // Save the user to the database
-    newUser.save((err, user) => {
+    form.parse(req, async (err, fields) => {
       if (err) {
         console.error(err);
-        res.status(500).send('Internal Server Error');
-      } else {
-        res.status(201).send('Registration successful');
+        return res.status(500).send('Internal Server Error');
       }
+
+      const { firstname, lastname, email, password } = fields;
+
+      if (!(firstname && lastname && email && password)) {
+        return res.status(400).send('Enter all the Credentials!!');
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user
+      const newUser = new User({
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword,
+      });
+
+      // Save the user to the database
+      newUser.save()
+        .then((user) => {
+          res.status(201).send('Registration successful');
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Internal Server Error');
+        });
     });
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 });
 
-// // // Login Endpoint
-// // app.post('/login', async (req, res) => {
-// //   try {
-// //     const { email, password } = req.fields;
+// Login Endpoint
 
-// //     // Find the user in the database
-// //     const user = await User.findOne({ email });
+app.post('/login', async (req, res) => {
 
-// //     if (!user) {
-// //       res.status(401).send('Invalid username or password');
-// //       return;
-// //     }
+  
+  try {
+    const form = new formidable.IncomingForm();
 
-// //     // Compare the provided password with the stored hash
-// //     const passwordMatch = await bcrypt.compare(password, user.password);
+    form.parse(req, async (err, fields) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+      }
 
-// //     if (passwordMatch) {
-// //       // Generate a JWT token
-// //       const token = jwt.sign({ username: user.username }, jwtSecret, { expiresIn: '1h' });
+      const { email, password } = fields;
 
-// //       res.status(200).json({ token });
-// //     } else {
-// //       res.status(401).send('Invalid username or password');
-// //     }
-// //   } catch (error) {
-// //     console.error(error);
-// //     res.status(500).send('Internal Server Error');
-// //   }
-// // });
+      // Find the user in the database
+      const user = await User.findOne({ email });
 
-// // // // Middleware to verify JWT token
-// // // const verifyToken = (req, res, next) => {
-// // //   const token = req.headers['authorization'];
+      if (!user) {
+        return res.status(401).send('Invalid username or password');
+      }
 
-// // //   if (!token) {
-// // //     return res.status(403).send('No token provided');
-// // //   }
+      
 
-// // //   jwt.verify(token, jwtSecret, (err, decoded) => {
-// // //     if (err) {
-// // //       return res.status(401).send('Failed to authenticate token');
-// // //     }
+      const passwordMatch = await bcrypt.compare(password, user.password);
 
-// // //     req.username = decoded.username;
-// // //     next();
-// // //   });
-// // // };
+      
 
-// // // Example protected route
-// // app.get('/profile', verifyToken, (req, res) => {
-// //   res.status(200).send(`Hello, ${req.username}!`);
-// // });
+      if (passwordMatch) {
+        // Generate a JWT token
+        
+        const token = jwt.sign({ email: user.email }, jwtSecret, { expiresIn: '1h' });
+       
 
-// Start the server
+        res.status(200).json({ token });
+      } else {
+        res.status(401).send('Invalid username or password');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).send('No token provided');
+  }
+
+  jwt.verify(token, jwtSecret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send('Failed to authenticate token');
+    }
+
+    req.firstname = decoded.firstname;
+    next();
+  });
+};
+
+// Example protected route
+app.get('/profile', verifyToken, (req, res) => {
+  res.status(200).send(`Hello, ${req.firstname}!`);
+});
+
+// Forgot Password Endpoint
+app.post('/forgot-password', async (req, res) => {
+  try {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, async (err, fields) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+      }
+
+      const { email } = fields;
+
+      // Find the user in the database
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(404).send('User not found');
+      }
+
+      // Generate reset token and expiry
+      const resetToken = crypto.randomBytes(20).toString('hex');
+      const resetTokenExpiry = Date.now() + 3600000; // Token expires in 1 hour
+
+      // Update user with reset token and expiry
+      await User.findByIdAndUpdate(user._id, { resetToken, resetTokenExpiry });
+
+      const resetLink = `http://localhost:3000/reset-password?token=${resetToken}`;
+
+      // Send password reset email
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'itd7557@gmail.com', // Replace with your email
+          pass: 'Pranaav@7447', // Replace with your email password
+        },
+      });
+
+      const mailOptions = {
+        from: 'itd7557@gmail.com', // Replace with your email
+        to: user.email,
+        subject: 'Password Reset Request',
+        html: `Click <a href="${resetLink}">here</a> to reset your password.`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send('Internal Server Error');
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.status(200).send('Password reset email sent');
+        }
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Reset Password Endpoint
+app.post('/reset-password', async (req, res) => {
+  try {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, async (err, fields) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+      }
+
+      const { token, newPassword } = fields;
+
+      // Find the user with the reset token and not expired
+      const user = await User.findOne({
+        resetToken: token,
+        resetTokenExpiry: { $gt: Date.now() },
+      });
+
+      if (!user) {
+        return res.status(401).send('Invalid or expired token');
+      }
+
+      // Update password
+      user.password = await bcrypt.hash(newPassword, 10);
+      user.resetToken = undefined;
+      user.resetTokenExpiry = undefined;
+
+      await user.save();
+
+      res.status(200).send('Password reset successful');
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
